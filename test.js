ec9d0c44bb7bbc56a5ce00cae07b9f4dedcdf8c3
shanchen <568906155@qq.com>
2024-12-28 02:48:39 +0800

    merge wtf

--- test.js
+++ test.js
@@ -2,8 +2,5 @@ console.log('add');
 console.log('change1');
 console.log('change2');
 console.log('change3');
-<<<<<<< HEAD
 console.log('master change');
-=======
 console.log('dev change');
->>>>>>> dev
