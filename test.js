25df6c83941a7beeb8a21cfd860a3e98c2f3ca6f
shanchen <568906155@qq.com>
2024-12-28 02:44:29 +0800

    dev2 change

--- test.js
+++ test.js
@@ -2,8 +2,6 @@ console.log('add');
 console.log('change1');
 console.log('change2');
 console.log('change3');
-<<<<<<< HEAD
 console.log('master change');
-=======
 console.log('dev change');
->>>>>>> dev
+console.log('dev2 change');
